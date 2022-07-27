419824a21dece345a8cce6be4c39d0cddd3aef28
thaian161 <thaian161@yahoo.com>
2022-07-27 01:17:37 -0500

    complete 6 one to many

--- exercises/ex_2_update_store1.rb
+++ exercises/ex_2_update_store1.rb
@@ -7,6 +7,10 @@ puts "----------"
 # Your code goes here ...
 @store1 = Store.find_by(id: 1)
 @store2 = Store.find_by(id: 2)
+@store3 = Store.find_by(id: 3)
+@store4 = Store.find_by(id: 4)
+@store5 = Store.find_by(id: 5)
+@store6 = Store.find_by(id: 6)
 
 @store1.update(name: "Chika Chika Boom Boom")
 
--- exercises/ex_6_one_to_many.rb
+++ exercises/ex_6_one_to_many.rb
@@ -9,3 +9,17 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Haha", last_name: "Noo", hourly_rate: 60)
+@store1.employees.create(first_name: "Davin", last_name: "Furtado", hourly_rate: 100)
+@store1.employees.create(first_name: "Geoff", last_name: "Ryan", hourly_rate: 80)
+
+@store2.employees.create(first_name: "Potato", last_name: "Wiggle Wiggle", hourly_rate: 150)
+@store2.employees.create(first_name: "Andy", last_name: "Lindsay", hourly_rate: 200)
+@store2.employees.create(first_name: "Hai", last_name: "Bui", hourly_rate: 700)
+
+@store4.employees.create(first_name: "Param", last_name: "Cem", hourly_rate: 88)
+
+@store5.employees.create(first_name: "Ann", last_name: "Bui", hourly_rate: 777)
+
+@store6.employees.create(first_name: "Whitney", last_name: "King", hourly_rate: 120)
+@store6.employees.create(first_name: "ThyAn", last_name: "BuiLee", hourly_rate: 320)
