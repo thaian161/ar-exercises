25046d8e0de6e973588a6027e22b5a1c52eac38b
thaian161 <thaian161@yahoo.com>
2022-07-27 00:29:23 -0500

    complete destory store 3

--- exercises/ex_3_delete_third_store.rb
+++ exercises/ex_3_delete_third_store.rb
@@ -2,7 +2,12 @@ require_relative '../setup'
 require_relative './ex_1_create_stores'
 require_relative './ex_2_update_store1'
 
-puts "Exercise 3"
+puts "Exercise 3: DELETE THE 3RD STORE"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(id: 3)
+
+@store3.destroy
+
+puts "Store count is now #{Store.count}"
